5ff0addb019d35999e4b795ecbba54a222ecb94f
Cloe Kouadjo <armelfli50@gmail.com>
2019-09-27 20:37:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,6 +15,9 @@ module FormsLab
       post '/pirates' do 
         p params
         @pirate = Pirate.new(name: params["pirate"]["name"], weight: params["pirate"]["weight"], height: params["pirate"]["height"])
+        @ships = params["pirate"]["ships"].each do |ship|
+          new_ship = Ship.new()
+        end
         erb :"pirates/show"
       end
 
